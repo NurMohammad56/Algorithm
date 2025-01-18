4c15ed0591aee93fb370c5e84578c381fa8a5136
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-18 23:21:07 +0600

    Roman to integer

---
+++ problem-12/index.js
@@ -0,0 +1,28 @@
+var romanToInt = function (s) {
+  const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+  let prevValue = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let currValue = romanValues[s[i]];
+
+    if (prevValue < currValue) {
+      total += currValue - 2 * prevValue;
+    } else {
+      total += currValue;
+    }
+
+    prevValue = currValue;
+  }
+
+  return total;
+};
